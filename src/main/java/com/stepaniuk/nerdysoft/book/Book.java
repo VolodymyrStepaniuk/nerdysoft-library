68f0f65ce0db18910e8b59e55386a6f352085f37
Volodymyr Stepaniuk <volodymyr.stepaniuk.04@gmail.com>
2024-08-17 00:14:03 +0300

    feat(book): Created Book entity and implemented it

---
+++ src/main/java/com/stepaniuk/nerdysoft/book/Book.java
@@ -0,0 +1,52 @@
+package com.stepaniuk.nerdysoft.book;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Entity
+@Table(name = "books")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "books_id_gen")
+    @SequenceGenerator(name = "books_id_gen", sequenceName = "books_id_seq", allocationSize = 1)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    @Column(name = "title", nullable = false)
+    private String title;
+    @Column(name = "author", nullable = false)
+    private String author;
+    @Column(name = "amount", nullable = false)
+    private Integer amount;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        Book book = (Book) o;
+        return getId() != null && Objects.equals(getId(), book.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + "(" +
+                "id = " + id + ", " +
+                "title = " + title + ", " +
+                "author = " + author + ", " +
+                "amount = " + amount + ")";
+    }
+}
