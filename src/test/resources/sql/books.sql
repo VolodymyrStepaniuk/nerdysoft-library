ee82727add3a2845e06a4e93d8e34cdbcdbccaac
Volodymyr Stepaniuk <volodymyr.stepaniuk.04@gmail.com>
2024-08-17 00:10:50 +0300

    test(sql): Added books.sql for use in JPA level tests

---
+++ src/test/resources/sql/books.sql
@@ -0,0 +1,6 @@
+INSERT INTO books (id, title, author, amount)
+VALUES (1, 'The Lord of the Rings', 'J.R.R. Tolkien', 1),
+       (2, 'Harry Potter', 'J.K. Rowling', 1),
+       (3, 'The Hobbit', 'J.R.R. Tolkien', 1),
+       (4, 'The Catcher in the Rye', 'J.D. Salinger', 1),
+       (5, 'To Kill a Mockingbird', 'Harper Lee', 1);
